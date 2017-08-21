ba68a3e9ceb8a6e6dd2763961e600d54c615420c
Manas Purohit <purohit.m@husky.neu.edu>
2017-08-20 19:09:51 -0700

    Initial commit

---
+++ app/src/main/java/com/manaspurohit/tictactoe/TicTacToeActivity.kt
@@ -0,0 +1,12 @@
+package com.manaspurohit.tictactoe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TicTacToeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe)
+    }
+}
